21e5f572cdafc248156a511703317963233970fe
brianmanoti <brianmanoti254@gmail.com>
2024-03-09 02:17:05 -0800

    Succesifully written a test program

--- assignment codes1/test_OSULL.cpp
+++ assignment codes1/test_OSULL.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 #include "OSULL.h"
 
 using namespace std;
 
+// Function to generate random integer within a given range
+int generateRandomInt(int min, int max) {
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
+
+// Function to measure the performance for a given capacity
+void measurePerformance(int capacity, const int listSize, const int numOperations) {
+    OSULL<int, 10> osull; // Assuming node capacity of 10 for now
+
+    clock_t start = clock();
+
+    for (int i = 0; i < numOperations; ++i) {
+        osull.insert(generateRandomInt(1, listSize * 10));
+        osull.find(generateRandomInt(1, listSize * 10));
+        osull.remove(generateRandomInt(1, listSize * 10));
+    }
+
+    clock_t end = clock();
+    double duration = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Time taken for " << (numOperations * 3) << " operations OSULL with node capacity " << capacity << ": " << duration * 1000 << " milliseconds" << endl;
+}
+
 int main() {
     const int listSize = 10000; // Fixed list size
     const int numOperations = 1000; // Number of operations for each test
-    const int nodeCapacities[] = {5, 10, 20}; // Varying node capacities for OSULL
+    constexpr int nodeCapacities[] = {5, 10, 20}; // Varying node capacities for OSULL
 
     srand(time(0)); // Seed the random number generator
 
-    for (int capacity : nodeCapacities) {
-        // Create an instance of OSULL with current node capacity
-        OSULL<int, capacity> osull;
-
-        // Populate OSULL with random integers
-        for (int i = 1; i <= listSize; ++i) {
-            osull.insert(i);
-        }
-
-        // Measure performance for OSULL with current node capacity
-        clock_t start = clock();
-
-        // Perform a series of operations (insertions, searches, removals)
-        for (int i = 0; i < numOperations; ++i) {
-            // Perform an insertion operation
-            osull.insert(rand() % (listSize * 10) + 1);
-
-            // Perform a search operation
-            osull.find(rand() % (listSize * 10) + 1);
-
-            // Perform a removal operation
-            osull.remove(rand() % (listSize * 10) + 1);
-        }
-
-        clock_t end = clock();
-        double duration = double(end - start) / CLOCKS_PER_SEC;
-
-        cout << "Time taken for " << (numOperations * 3) << " operations on OSULL with node capacity " << capacity << ": " << duration * 1000 << " milliseconds" << endl;
+    for (int i = 0; i < sizeof(nodeCapacities) / sizeof(nodeCapacities[0]); ++i) {
+        measurePerformance(nodeCapacities[i], listSize, numOperations);
     }
 
     return 0;
 }
+
---
+++ assignment codes1/test_OSULL2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include "OSULL.h"
+
+using namespace std;
+
+// Function to generate random integer within a given range
+int generateRandomInt(int min, int max) {
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<int> dist(min, max);
+    return dist(gen);
+}
+
+// Function to measure the performance for a given capacity
+template <int Capacity>
+void measurePerformance(const int listSize, const int numOperations) {
+    OSULL<int, Capacity> osull;
+
+    clock_t start = clock();
+
+    for (int i = 0; i < numOperations; ++i) {
+        osull.insert(generateRandomInt(1, listSize * 10));
+        osull.find(generateRandomInt(1, listSize * 10));
+        osull.remove(generateRandomInt(1, listSize * 10));
+    }
+
+    clock_t end = clock();
+    double duration = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Time taken for " << (numOperations * 3) << " operations OSULL with node capacity " << Capacity << ": " << duration * 1000 << " milliseconds" << endl;
+}
+
+int main() {
+    const int listSize = 10000; // Fixed list size
+    const int numOperations = 1000; // Number of operations for each test
+    constexpr int nodeCapacities[] = {5, 10, 20}; // Varying node capacities for OSULL
+
+    srand(time(0)); // Seed the random number generator
+
+    for (int i = 0; i < sizeof(nodeCapacities) / sizeof(nodeCapacities[0]); ++i) {
+        measurePerformance<nodeCapacities[i]>(listSize, numOperations);
+    }
+
+    return 0;
+}
+
